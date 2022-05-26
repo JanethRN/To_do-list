0edfa0db35d1fd5da7c85b0ca3375fa10b039e34
JanethRN <jjrenteria@espe.edu.ec>
2022-05-25 20:34:42 -0500

    third commit

--- todoapp.py
+++ todoapp.py
@@ -1,26 +1,37 @@
 #Se importa la librería Flask
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, flash, url_for
 
 #variable de instancia app
 app=Flask(__name__, template_folder='templates')
 
-#Ruta raíz
+
+#configuración
+app.secret_key = 'mysecretkey'
+#agregamos una variable tipo array
+tareas = []
+
+#decorador ruta raíz
 @app.route('/')
-#función que retorna la página
+#función que retorna la página principal 
 def principal():
-    return render_template('index.html')
+    return render_template('index.html', tareas=tareas)
 
 
-#Ruta página de inicio html
-@app.route('/enviar')
-
-#función que retorna la página
+#Ruta página de envio, permite el envio de datos
+@app.route('/enviar', methods=['POST'])
 def enviar():
-    return render_template('enviar.html')
-
-
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        correo_electronico = request.form['correo_electronico']
+        prioridad = request.form['prioridad']
+        if descripcion == '' or correo_electronico =='' or prioridad=='seleccionar':
+            flash('Ingresar todos los campos')
+            return redirect(href={ url_for('principal') })
+        else: 
+            flash('Se agrego correctamente!')
+        
 #main del programa
 if __name__ == '__main__':
+    #puerto 5000, debug cada vez que cambiamos dentro del servidor se reinicia automaticamente
+    app.run(port = 5000, debug = True)
 
-# debug= True, para reiniciar automaticamente el servidor
-    app.run(debug=True)
